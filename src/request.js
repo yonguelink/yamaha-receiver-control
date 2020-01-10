8d5d1549d5b00fef088a2113ac1f8156256a352b
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2020-01-10 15:27:33 -0500

    Don't reboot the receiver if the change input wasn't called in the last 5 minutes

--- src/request.js
+++ src/request.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const { promisify } = require('util');
+const fs = require('fs-extra');
+const path = require('path');
+const moment = require('moment');
+
 const doWait = promisify(setTimeout);
 
 const receiverUrl = 'http://192.168.1.13/YamahaRemoteControl/ctrl';
@@ -7,6 +11,7 @@ const config = {
   headers: { 'Content-Type': 'text/xml' },
   responseType: 'text'
 };
+const lastRunFile = path.join(__dirname, '..', 'last-run.log');
 
 async function doRequest (request) {
   return axios.post(receiverUrl, request, config);
@@ -28,12 +33,31 @@ function buildVolumeRequest (volume) {
   return `<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>-${volume}0</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>`;
 }
 
+async function updateLastTimeRan () {
+  await fs.ensureDir(path.dirname(lastRunFile));
+  await fs.writeFile(lastRunFile, moment().format('YYYY-MM-DDTHH:mm:ss'));
+}
+
+async function shouldPowerOff () {
+  if (await fs.pathExists(lastRunFile)) {
+    const data = await fs.readFile(lastRunFile, 'utf8');
+    const lastRunMoment = moment(data.replace('\n', ''), 'YYYY-MM-DDTHH:mm:ss', true);
+    return !lastRunMoment.isBefore(moment().subtract(5, 'minutes'));
+  }
+  return true;
+}
+
 module.exports.changeInput = async function (input, volume) {
-  await doRequest(powerOffRequest());
-  await doWait(500);
+  if (await shouldPowerOff()) {
+    await doRequest(powerOffRequest());
+    await doWait(500);
+  }
+
   await doRequest(powerOnRequest());
   await doRequest(buildInputRequest(input));
   await doRequest(buildVolumeRequest(volume));
+
+  await updateLastTimeRan();
 };
 
 module.exports.turnOff = async function () {
